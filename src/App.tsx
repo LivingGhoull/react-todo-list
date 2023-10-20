39e557b78f5b5a5973bd933acb3ae711c6ab0f3a
LivingGhoull <65529392+LivingGhoull@users.noreply.github.com>
2023-10-20 12:32:21 +0200

    feat - delete todo and complete todo
    Now we can delete a todo and it will not take other then it self
    
    When a task is complete it gets an extra class to style

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
-import { v4 as uuidv4 } from 'uuid';
+import { v4 as uuidv4 } from "uuid";
 import { useState } from "react";
 import TodoForm from "./components/TodoForm";
+import Todo from "./components/Todo";
 
 type TodoItem = {
   id: string;
@@ -11,18 +12,27 @@ function App() {
   const [todoList, setTodoList] = useState<TodoItem[]>([]);
 
   const addTodo = (todo: string) => {
-    setTodoList([...todoList, { id: uuidv4(), todo: todo }]);
+    setTodoList([...todoList, { id: uuidv4(), todo: todo}]);
+  };
+
+  const deleteTodo = (id: string) => {
+    setTodoList(todoList.filter((ob) => ob.id !== id));
   };
 
   return (
     <div>
       <h1>To do list</h1>
       <TodoForm addTodo={addTodo} />
-      {todoList.map((todo: TodoItem) => 
-      
-      <p key={todo.id}>{todo.todo}</p>
-            
-      )}
+      {todoList.map((todo: TodoItem, index) => (
+        <Todo
+          key={todo.id}
+          index={index}
+          id={todo.id}
+          deleteTodo={deleteTodo}
+        >
+          {todo.todo}
+        </Todo>
+      ))}
     </div>
   );
 }
--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -1,7 +1,26 @@
+import { useState } from "react"
+import "../style/todo.css"
 
-function Todo() {
+interface Props {
+  index: number
+  id: string
+  children: string
+  deleteTodo: (id: string) => void
+}
+
+function Todo({index, id, children, deleteTodo}: Props) {
+  const [check, setCheck] = useState(false)
+  
+  const handleCheck = () => {
+    setCheck(!check)
+  }
+
   return (
-    <div>Todo</div>
+    <div className={check ? "todo-item complete" : "todo-item"}>
+      <input id={"todo-"+index} type="checkbox" onClick={handleCheck}/>
+      <label htmlFor={"todo-"+index}>{children}</label>
+      <button onClick={() => deleteTodo(id)}>Delete</button>
+    </div>
   )
 }
 
